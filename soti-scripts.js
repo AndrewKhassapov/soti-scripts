f67c675aa4343b901281832ddfbc71900c151e22
Andrew Khassapov <53222142+AndrewKhassapov@users.noreply.github.com>
2022-05-03 16:05:08 +1000

    Update soti-scripts.js
    Updated SOTI MobiControl API javascript wrapper with use cases.
    Tested on 350x Honeywell devices. Test passed.

--- soti-scripts.js
+++ soti-scripts.js
@@ -1,11 +1,12 @@
 /**
- * SOTI MobiControl file editing javascript base
+ * SOTI MobiControl file editing javascript wrapper
  * @author Andrew Khassapov
  * @version 1.0.0
  * @example In SOTI MobiControl: Select 'Send Script' > Tick the 'JavaScript' box > Paste script into Script Editor > Press 'SEND SCRIPT'.
  *          '/mnt/sdcard/example_file.txt'
  * @see {@link https://www.soti.net/mc/help/javascriptapi/en/index.html} Refer to the SOTI MobiControl Android Agent JavaScript API
  *      Note SOTI API does not support 'let'. Use 'var' instead.
+ *      Note SOTI API does not support string literal variables, `${var}`. Use "+var+" instead.
  */
 
 // Example: backupFile('/sdcard/honeywell/persist/DeviceConfig.xml', '/sdcard/honeywell/persist/DeviceConfig.xml.bak');
@@ -19,6 +20,7 @@ function backupFile(fileOriginal, fileNew) {
     var destination = new mobicontrol.io.File(fileNew);
     source.copyTo(destination);
 }
+// Ad hoc use cases:
 backupFile('/sdcard/honeywell/persist/DeviceConfig.xml', '/sdcard/honeywell/persist/DeviceConfig.xml.bak');
 
 // Example: createFile('/mnt/sdcard/example_file.txt');
@@ -26,13 +28,14 @@ function createFile(fileFull) {
     let file = new mobicontrol.io.File(fileFull);
     if (file.exists) {
         mobicontrol.log.info(`File ` + fileFull + ` exists`);
+        return;
     } else {
         file.createFile();
     }
 }
 
 // Example: editFile('/sdcard/honeywell/persist/DeviceConfig.xml', '<Key name="screen_off_timeout">1800000</Key>', '<Key name="screen_off_timeout">9000</Key>');
-function editFile(fileFull, findText, replaceText)
+function editFile(fileFull, findText, replacementText)
 {
     var file = new mobicontrol.io.File(fileFull);
     if(!file.exists){
@@ -40,21 +43,19 @@ function editFile(fileFull, findText, replaceText)
         return;
     }
 
-
+    var dataIn = file.readText();
+    var dataOut = dataIn.replace(findText, replacementText);
+    
+    var outputFile = new mobicontrol.io.File(fileFull);
+    outputFile.writeText(dataOut);
 }
-/*const fileName = 'DeviceConfig.xml';
-const fileDir = '/mnt/sdcard/honeywell/persist/';
-var fileFull = fileDir.toString() + fileName.toString();
+// Ad hoc use cases:
+editFile('/sdcard/honeywell/persist/DeviceConfig.xml','<Key name="SelectTimeZone">Australia/Sydney</Key>','');
+editFile('/sdcard/honeywell/persist/DeviceConfig.xml','<Key name="SelectTimeZone">Australia/Brisbane</Key>','');
 
-var inputFile;
-try{
-    inputFile = new mobicontrol.io.File(fileFull);
-} catch(e)
-{}
-
-var data = inputFile.readText();
-//var result = data.replace(/yahoo.com/g, 'google.com');
-
-var outputFile = new mobicontrol.io.File(‘someFile’);
-outputFile.writeText(result);
-*/
+// Example: honeywellSection("Provisioning Mode") returns everything between <Section name="Provisioning Mode"></Section>
+function honeywellSection(sectionName)
+{
+    var regex = new RegExp('(?<=<Section\sname="'+sectionName+'">).*?(?=\s+<\/Section>)', 'gsi');
+    return regex;
+}
