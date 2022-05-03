7cad85ca8e269423425f4dbeca8093049f0bad2b
Andrew Khassapov <53222142+AndrewKhassapov@users.noreply.github.com>
2022-05-03 11:43:22 +1000

    Testing SOTI MobiControl API
    Issue: 'let' keyword not accepted by SOTI API.
    Replaced all 'let' with 'var'.
    Notes added.

--- soti-scripts.js
+++ soti-scripts.js
@@ -5,6 +5,7 @@
  * @example In SOTI MobiControl: Select 'Send Script' > Tick the 'JavaScript' box > Paste script into Script Editor > Press 'SEND SCRIPT'.
  *          '/mnt/sdcard/example_file.txt'
  * @see {@link https://www.soti.net/mc/help/javascriptapi/en/index.html} Refer to the SOTI MobiControl Android Agent JavaScript API
+ *      Note SOTI API does not support 'let'. Use 'var' instead.
  */
 
 // Example: backupFile('/sdcard/honeywell/persist/DeviceConfig.xml', '/sdcard/honeywell/persist/DeviceConfig.xml.bak');
@@ -18,10 +19,11 @@ function backupFile(fileOriginal, fileNew) {
     var destination = new mobicontrol.io.File(fileNew);
     source.copyTo(destination);
 }
+backupFile('/sdcard/honeywell/persist/DeviceConfig.xml', '/sdcard/honeywell/persist/DeviceConfig.xml.bak');
 
 // Example: createFile('/mnt/sdcard/example_file.txt');
 function createFile(fileFull) {
-    var file = new mobicontrol.io.File(fileFull);
+    let file = new mobicontrol.io.File(fileFull);
     if (file.exists) {
         mobicontrol.log.info(`File ` + fileFull + ` exists`);
     } else {
@@ -29,6 +31,17 @@ function createFile(fileFull) {
     }
 }
 
+// Example: editFile('/sdcard/honeywell/persist/DeviceConfig.xml', '<Key name="screen_off_timeout">1800000</Key>', '<Key name="screen_off_timeout">9000</Key>');
+function editFile(fileFull, findText, replaceText)
+{
+    var file = new mobicontrol.io.File(fileFull);
+    if(!file.exists){
+        mobicontrol.log.info(`Error: File does not exist: ` + fileOriginal);
+        return;
+    }
+
+
+}
 /*const fileName = 'DeviceConfig.xml';
 const fileDir = '/mnt/sdcard/honeywell/persist/';
 var fileFull = fileDir.toString() + fileName.toString();
