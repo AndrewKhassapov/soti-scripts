f0b537134daf361d94842a7efac6aaf0b6ce3858
Andrew Khassapov <53222142+AndrewKhassapov@users.noreply.github.com>
2022-05-04 09:35:00 +1000

    Update soti-scripts.js
    Resolved issue with 'let' not being supported by SOTI API.
    Using 'var' until updates to SOTI API.

--- soti-scripts.js
+++ soti-scripts.js
@@ -1,7 +1,7 @@
 /**
  * SOTI MobiControl file editing javascript wrapper
  * @author Andrew Khassapov
- * @version 1.0.0
+ * @version 1.0.1
  * @example In SOTI MobiControl: Select 'Send Script' > Tick the 'JavaScript' box > Paste script into Script Editor > Press 'SEND SCRIPT'.
  *          '/mnt/sdcard/example_file.txt'
  * @see {@link https://www.soti.net/mc/help/javascriptapi/en/index.html} Refer to the SOTI MobiControl Android Agent JavaScript API
@@ -11,21 +11,21 @@
 
 // Example: backupFile('/sdcard/honeywell/persist/DeviceConfig.xml', '/sdcard/honeywell/persist/DeviceConfig.xml.bak');
 function backupFile(fileOriginal, fileNew) {
-    var source = new mobicontrol.io.File(fileOriginal);
-    if(!source.exists){
+    var file = new mobicontrol.io.File(fileOriginal);
+    if(!file.exists){
         mobicontrol.log.info(`Error: File does not exist: ` + fileOriginal);
         return;
     }
 
     var destination = new mobicontrol.io.File(fileNew);
-    source.copyTo(destination);
+    file.copyTo(destination);
 }
 // Ad hoc use cases:
 //backupFile('/sdcard/honeywell/persist/DeviceConfig.xml', '/sdcard/honeywell/persist/DeviceConfig.xml.bak');
 
 // Example: createFile('/mnt/sdcard/example_file.txt');
 function createFile(fileFull) {
-    let file = new mobicontrol.io.File(fileFull);
+    var file = new mobicontrol.io.File(fileFull);
     if (file.exists) {
         mobicontrol.log.info(`Error: File already exists: ` + fileFull);
         return;
